b42cec27b7bd74f4c583397e10bf50fc7d0f64d2
janghyeuk-pixel <janghyeuk@gmail.com>
2026-04-30 02:40:54 +0900

    deploy build to docs

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: process.env.VERCEL ? '/' : '/taelimelectronics/',
+  base: '/taelimelectronics/',  // process.env.VERCEL ? '/' : '/taelimelectronics/',
   plugins: [react()],
 })
 
