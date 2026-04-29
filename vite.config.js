f79b89be230d0daa13cc7d7768668898bb57d2ac
janghyeuk-pixel <janghyeuk@gmail.com>
2026-04-30 06:12:58 +0900

    Deploy static site

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  base: '/taelimelectronics/',  // process.env.VERCEL ? '/' : '/taelimelectronics/',
-  plugins: [react()],
-})
-
